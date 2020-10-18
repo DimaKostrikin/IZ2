ac9802038b697f6a4066dfb31e6022de93e0aa0c
Dmitry Kostrikin <dima.da222@yandex.ru>
2020-10-18 04:50:32 +0300

    123

--- src/main.c
+++ src/main.c
@@ -1,9 +1,9 @@
 #include "IZ2/utils.h"
 
-// ./main.out file1.txt 100
+// ./main.out file_vect.txt file_base.txt 100
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
+    if (argc != 4) {
         fprintf(stderr, "3 arguments: vector, base, num of components\n");
         return 1;
     }
@@ -15,23 +15,23 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    /*FILE *file_base = fopen(argv[2], "r");
+    FILE *file_base = fopen(argv[2], "r");
 
     if (!file_base) {
         fprintf(stderr, "File of base not found. Error.\n");
         fclose(file_vect);
         return 1;
     }
-    */
-    int vector_size = atoi(argv[2]);
+
+    int vector_size = atoi(argv[3]);
 
     if (vector_size < 1) {
         fprintf(stderr, "Vector size can't be 0 or less\n");
         fclose(file_vect);
-        //fclose(file_base);
+        fclose(file_base);
     }
 
     double *vect = read_vector(vector_size, file_vect);
     double norm = vector_norm(vect, vector_size);
-    printf("%lf", norm);
+    printf("%lf\n", norm);
 }
